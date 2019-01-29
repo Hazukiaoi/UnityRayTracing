5bb1786b27e844122de29607136973955bb54a1b
hazukiaoi\n64com <hazuki.aoi1@gmail.com>
2019-01-29 18:17:56 +0800

    已经获得相机的四个角，回去改用左下角+相机右向量+相机上向量的方式进行计算屏幕像素

--- Assets/Scripts/RaycastTest.cs
+++ Assets/Scripts/RaycastTest.cs
@@ -9,29 +9,75 @@ public class RaycastTest : MonoBehaviour
     Mesh mesh;
 
     List<Ray> rays;
+
+    Vector3[] cameraCorn;
+    Vector3 cameraPosition;
+
+    [Range(0,1)]
+    public float h = 0;
+
+    [Range(0, 1)]
+    public float v = 0;
     // Use this for initialization
     void Start()
     {
+        SetUpScene();
+        GetCameraData();
+    
+        rays = new List<Ray>();
 
+    }
+
+    /// <summary>
+    /// 初始化场景
+    /// </summary>
+    void SetUpScene()
+    {
         mesh = new Mesh();
         //初始化场景
         MeshFilter[] _mfs = FindObjectsOfType<MeshFilter>();
         CombineInstance[] _cin = new CombineInstance[_mfs.Length];
-        for(int i = 0; i < _mfs.Length; i++)
+        for (int i = 0; i < _mfs.Length; i++)
         {
             _cin[i].mesh = _mfs[i].mesh;
             _cin[i].transform = _mfs[i].transform.localToWorldMatrix;
         }
         mesh.CombineMeshes(_cin);
-
-        rays = new List<Ray>();
     }
 
+    /// <summary>
+    /// 初始化相机
+    /// </summary>
+    void GetCameraData()
+    {
+        Camera cam = Camera.main;
+        cameraCorn = new Vector3[4];
+        cam.CalculateFrustumCorners(new Rect(0, 0, 1, 1), cam.nearClipPlane, Camera.MonoOrStereoscopicEye.Mono, cameraCorn);    
+        cameraPosition = cam.transform.position;
+        for (int i = 0; i < 4; i++)
+        {
+            cameraCorn[i] = cam.transform.localToWorldMatrix * cameraCorn[i];
+        }
+    }
 
     Color[] _c = new Color[] { Color.black, Color.red, Color.yellow, Color.green, Color.cyan };
 
-	void Update ()
+    void Update ()
     {
+
+        for (int i = 0;i < 4; i++)
+        {
+            Debug.DrawRay(cameraPosition, cameraCorn[i], Color.blue);
+
+        }
+
+        Vector3 _hd = Vector3.Lerp(cameraCorn[0], cameraCorn[3], h);
+        Vector3 _ht = Vector3.Lerp(cameraCorn[1], cameraCorn[2], h);
+        Vector3 _c = Vector3.Lerp(_hd, _ht, v);
+
+        Debug.DrawRay(cameraPosition, _c.normalized, Color.red);
+
+        return;
 
         rays.Clear();
         Ray startRay = new Ray(transform.position, transform.forward);
@@ -70,12 +116,13 @@ public class RaycastTest : MonoBehaviour
                     ref u,
                     ref v))
                 {
+                    //当目标位于正方向
                     if(t > 0)
                     {
                         if (t < _cDistance)
                         {
                             normal = (Vector3.Lerp(mesh.normals[cPoint_0], mesh.normals[cPoint_1], u) + Vector3.Lerp(mesh.normals[cPoint_0], mesh.normals[cPoint_2], v)) / 2;
-                            castPoint = rays[step].GetPoint(t) + normal *0.0001f;                   
+                            castPoint = rays[step].GetPoint(t) + normal * 1e-5f;              //不添加上偏移量会导致错误的cast     
                             isCast = true;
                             _cDistance = t;
                         }
@@ -90,16 +137,16 @@ public class RaycastTest : MonoBehaviour
                 if(step +1 < MAX_STEP)
                 {
                     rays.Add(new Ray(castPoint, Vector3.Reflect(rays[step].direction, normal).normalized));
-                    Debug.DrawRay(castPoint, normal, Color.blue);
+                    //Debug.DrawRay(castPoint, normal, Color.blue);
                 }
             }
 
             isCastPrv = isCast;
         }
 
-        for(int i = 0;i < rays.Count ; i++)
-        {
-            Debug.DrawRay(rays[i].origin, rays[i].direction * 5, _c[i]);
-        }
+        //for(int i = 0;i < rays.Count ; i++)
+        //{
+        //    Debug.DrawRay(rays[i].origin, rays[i].direction * 5, _c[i]);
+        //}
 	}
 }
